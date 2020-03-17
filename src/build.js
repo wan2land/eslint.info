80f7de7941218bbf1f394e7e6ff550491485ef1a
Changwan Jun <wan2land@gmail.com>
2020-03-18 02:23:58 +0900

    Add template, rules

--- src/build.js
+++ src/build.js
@@ -4,20 +4,40 @@ const { CLIEngine } = require('eslint')
 const rules = require('eslint/lib/rules')
 
 const configs = [
-  'airbnb',
+  { name: 'eslint:recommended', slug: 'eslint__recommended' },
+  { name: 'Airbnb', slug: 'airbnb' },
+  { name: 'Google', slug: 'google' },
+  { name: 'Standard', slug: 'standard' },
+  { name: 'Semistandard', slug: 'semistandard' },
+  { name: 'XO', slug: 'xo' },
 ]
 
 module.exports = {
   build() {
     return {
-      configs: configs.map((name) => ({
+      configs: configs.map(({ name, slug }) => ({
         name,
-        config: new CLIEngine().getConfigForFile(path.resolve(__dirname, `../eslint-configs/${name}/.eslintrc.js`)),
+        config: new CLIEngine().getConfigForFile(path.resolve(__dirname, `../eslint-configs/${slug}/.eslintrc.js`)),
       })),
-      rules: [...rules.entries()].map(([name, { meta }]) => ({
-        name,
-        description: meta.docs.description,
-      })),
+      categories: [...[...rules.entries()]
+        .map(([name, { meta }]) => ({
+          name,
+          description: meta.docs.description,
+          url: meta.docs.url,
+          category: meta.docs.category,
+          recommended: meta.docs.recommended,
+          fixable: meta.fixable,
+        }))
+        .reduce((carry, rule) => {
+          carry.has(rule.category) || carry.set(rule.category, [])
+          carry.get(rule.category).push(rule)
+          return carry
+        }, new Map())
+        .entries()]
+        .map(([name, rules]) => ({
+          name,
+          rules: rules.sort((a, b) => a.name > b.name ? 1 : -1),
+        }))
     }
   },
 }
